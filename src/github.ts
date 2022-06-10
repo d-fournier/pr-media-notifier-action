28d367980827bc16fba933a06f8c4cf8c2d8e416
dfournier <pro@dfournier.me>
2022-06-10 19:40:54 +0200

    Update compiled binaries

--- src/github.ts
+++ src/github.ts
@@ -1,11 +1,11 @@
 import * as github from '@actions/github'
 
 export async function getCurrentPRDescription(
-  token: string,
+  token: string
 ): Promise<string | null> {
   const issueNumber = github.context.issue.number
   if (issueNumber != null) {
-    const { data: pullRequest } = await github.getOctokit(token).rest.pulls.get({
+    const {data: pullRequest} = await github.getOctokit(token).rest.pulls.get({
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
       pull_number: issueNumber
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
-import { getCurrentPRDescription } from './github'
-import { notify } from './slack'
+import {getCurrentPRDescription} from './github'
+import {notify} from './slack'
 
 async function run(): Promise<void> {
   try {
@@ -9,7 +9,8 @@ async function run(): Promise<void> {
     const token: string = core.getInput('github-token')
 
     const issueNumber = github.context.issue.number
-    const message = await getCurrentPRDescription(token) || `No description or PR`
+    const message =
+      (await getCurrentPRDescription(token)) || `No description or PR`
     await notify(webhook, message)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
